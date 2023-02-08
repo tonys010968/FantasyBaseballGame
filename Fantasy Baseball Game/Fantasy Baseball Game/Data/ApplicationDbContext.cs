4674ea73528ed540ccd15f6c9ba953aeefe4f647
Jacyn <anthony@thesavocas.com>
2023-02-07 20:26:32 -0500

    Added NuGets and Player Data Table

---
+++ Fantasy Baseball Game/Fantasy Baseball Game/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Fantasy_Baseball_Game.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Player> Players { get; set; }
+
+    }
+}
---
+++ Fantasy Baseball Game/Fantasy Baseball Game/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fantasy_Baseball_Game.Models
+{
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public string Position { get; set; }
+
+        public float Salary { get; set; }
+    }
+}
